d6d6aff527ef2b9201a64ea47b6faad93d7478aa
Eugene <eugenekozhenevskiy@gmail.com>
2024-05-29 18:46:17 +0300

    add urls for config app

--- config/urls.py
+++ config/urls.py
@@ -15,8 +15,16 @@ Including another URLconf
     2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
 """
 from django.contrib import admin
-from django.urls import path
+from django.urls import path, include
+from django.conf import settings
+from django.conf.urls.static import static
+from django.views.generic import RedirectView
+
 
 urlpatterns = [
-    path('admin/', admin.site.urls),
-]
+    path("admin/", admin.site.urls),
+    path("accounts/", include("django.contrib.auth.urls")),
+    path("", RedirectView.as_view(url="tasks/", permanent=True)),
+    path("tasks/", include("task.urls", namespace="task")),
+    path("employees/", include("employee.urls", namespace="employee"))
+] + static(settings.STATIC_URL, document_root=settings.STATIC_ROOT)
