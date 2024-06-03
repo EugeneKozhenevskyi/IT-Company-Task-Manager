32926b57b6a7b837387700f751b75ae076c71ed4
Eugene <eugenekozhenevskiy@gmail.com>
2024-06-03 12:56:15 +0300

    add tests for forms & models & delete views tests

--- tests/tests_forms.py
+++ tests/tests_forms.py
@@ -1,6 +1,10 @@
 from django.test import TestCase
-from employee.forms import EmployeeCreateForm
+from employee.forms import (EmployeeCreateForm,
+                            EmployeeUsernameSearchForm)
 from employee.models import Position
+from datetime import date, timedelta
+from task.forms import TaskCreateForm, Task
+from task.models import TaskType, Task
 
 
 class EmployeeFormTest(TestCase):
@@ -13,14 +17,42 @@ class EmployeeFormTest(TestCase):
             "password2": "Stastest123*",
             "first_name": "Stanislav",
             "last_name": "Cshovich",
-            "position": position.id,
+            "position": position,
         }
         form = EmployeeCreateForm(data=form_data)
         self.assertTrue(form.is_valid())
+        self.assertEqual(form.cleaned_data, form_data)
 
-        cleaned_data = form.cleaned_data
-        self.assertEqual(cleaned_data["username"], form_data["username"])
-        self.assertEqual(cleaned_data["email"], form_data["email"])
-        self.assertEqual(cleaned_data["first_name"], form_data["first_name"])
-        self.assertEqual(cleaned_data["last_name"], form_data["last_name"])
-        self.assertEqual(cleaned_data["position"], position)
+    def test_employee_search_form_is_valid(self):
+        form_data = {
+            "username": "Stasyan",
+        }
+        form = EmployeeUsernameSearchForm(data=form_data)
+        self.assertTrue(form.is_valid())
+
+
+class TaskFormTest(TestCase):
+    def setUp(self):
+        self.task_data = {
+            "name": "Eugene",
+            "task_type": TaskType.objects.create(name="TestTask"),
+            "priority": 1,
+            "description": "Test Task",
+            "deadline": date.today() - timedelta(days=1),
+        }
+
+    def test_invalid_deadline(self):
+        form = TaskCreateForm(data=self.task_data)
+        self.assertFalse(form.is_valid())
+
+    def test_valid_deadline(self):
+        self.task_data["deadline"] = date.today() + timedelta(1)
+        form = TaskCreateForm(data=self.task_data)
+        self.assertTrue(form.is_valid())
+        form.save()
+
+        self.assertEqual(
+            Task.objects.get(id=1).name,
+            self.task_data["name"]
+        )
+
