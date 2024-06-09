b57e9b15d9b8c6ecc040487f6ef80596af46bf92
Eugene <eugenekozhenevskiy@gmail.com>
2024-06-09 12:55:50 +0300

    remove http from allowed hosts

--- config/settings.py
+++ config/settings.py
@@ -12,7 +12,7 @@ SECRET_KEY = os.getenv("DJANGO_SECRET_KEY")
 
 DEBUG = os.environ.get("DJANGO_DEBUG", "") != "False"
 
-ALLOWED_HOSTS = ["127.0.0.1", "https://it-company-task-manager-uzrc.onrender.com/"]
+ALLOWED_HOSTS = ["127.0.0.1", "it-company-task-manager-uzrc.onrender.com"]
 
 INTERNAL_IPS = [
     "127.0.0.1",
